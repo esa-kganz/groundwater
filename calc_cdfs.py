dfeaab7418afa40be23f8c9dc13b1a152a384f60
Keenan Ganz <KGanz@esassoc.com>
2020-07-15 10:04:18 -0700

    add r squared to regression output, add output location

--- calc_cdfs.py
+++ calc_cdfs.py
@@ -13,6 +13,8 @@ import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 
+OUTPUT_LOC = r'U:\GIS\temp\KJG\fluccs\wells_output.csv"
+
 def sanitize_latlon(x):
     '''
     Converts concatenated coordinates of the form
@@ -28,11 +30,23 @@ def sanitize_latlon(x):
 
     return deg + (min / 60) + (sec / 3600)
 
+
 def sigmoid(x, a, b):
     '''
     Compute a logistic curve.
     '''
     return 1.0 / (1 + np.exp(-b * (x-a)))
+
+
+def r_squared(a, b):
+    '''
+    Get the proportion of variance in a explained by b.
+    '''
+    SS_tot = np.sum(np.square(a - np.mean(a)))
+    SS_res = np.sum(np.square(a - b))
+
+    return 1 - (SS_res / SS_tot)
+
 
 def get_cdf_parameters(x):
     '''
@@ -44,29 +58,28 @@ def get_cdf_parameters(x):
     x_sort = np.sort(x)
     cdf = np.array(range(N)) / float(N)
 
-    popt, _ = curve_fit(sigmoid, x_sort, cdf)
-    return popt
+    (a, b), _ = curve_fit(sigmoid, x_sort, cdf)
+    r_sq = r_squared(cdf, sigmoid(x_sort, a, b))
+    return a, b, r_sq
+
+
+def process_df(row):
+    this_df = pd.read_csv('data/csvs/{}.csv'.format(row['Dbkey']))
+    gwater_depths = -(this_df['gwater_elev_navd88'] -
+                      row['Elev_NAVD88']).dropna().to_numpy()
+    return get_cdf_parameters(gwater_depths)
+
 
 if __name__ == '__main__':
     wells = pd.read_csv('wells.csv')
-    a_lis = []
-    b_lis = []
+    coeffs = wells.apply(
+        process_df,
+        axis=1,
+        result_type='expand').rename(
+        columns={0: 'sigmoid_a', 1: 'sigmoid_b', 2: 'sigmoid_r2'}
+    )
 
-    for idx, row in wells.iterrows():
-        this_df = pd.read_csv('data/csvs/{}.csv'.format(row['Dbkey']))
-        gwater_depths = -(this_df['gwater_elev_navd88'] - row['Elev_NAVD88']).dropna().to_numpy()
-        a, b = get_cdf_parameters(gwater_depths)
-        a_lis.append(a)
-        b_lis.append(b)    
+    # Join the two dataframes together, rename last
+    all_data = pd.concat([wells, coeffs], axis=1)
 
-    all_data = pd.DataFrame({
-        'dbkey': wells['Dbkey'],
-        'latitude': wells['Latitude'].map(sanitize_latlon),
-        'longitude': wells['Longitude'].map(sanitize_latlon) * -1, # E to W
-        'x_coord': wells['X COORD'],
-        'y_coord': wells['Y COORD'],
-        'sigmoid_a': a_lis,
-        'sigmoid_b': b_lis
-    })
-
-    all_data.to_csv('output.csv')
+    all_data.to_csv(OUTPUT_LOC)
